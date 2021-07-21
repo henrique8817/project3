12ca44dc41afcfbd7619d7c96ac1fdf1d036eba2
henrique8817 <lucymonteiro.gata@gmail.com>
2021-07-21 12:24:36 -0300

    primeiro commit

---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import P from 'prop-types';
+import Head from 'next/head';
+import styled from 'styled-components';
+import config from '../config';
+import { mapData } from '../api/map-data';
+import Home from '../templates/Home';
+import { loadPages } from '../api/load-pages';
+
+const Heading = styled.h1`
+   background: ${({ theme }) => theme.colors.secondaryColor};
+`;
+
+export default function Index({ data = null }) {
+  return (
+    <Home data={data} />
+  )
+}
+
+export const getStaticProps = async () => {
+   let data = null;
+
+   try {
+     data = await loadPages('landing-page'); 
+   } catch(e) {
+      //
+   }
+
+   if(!data || !data.length) {
+      return {
+        notFound: true,
+      };
+   }
+
+   return {
+      props: {
+        data,
+      },
+   };
+};
+
+Index.protoTypes = {
+  data: P.array,
+};
